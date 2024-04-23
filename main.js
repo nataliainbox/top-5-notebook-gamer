1566a0b4e5ba6bdeaff49057ec1d5a6a2e7a11d7
Maria Lima <127319517+MeduuLima@users.noreply.github.com>
2024-04-23 07:59:59 -0300

    aaa

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = documents.querySelectorAll
 console.log(textos)
-
 for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
     botoes[i].onclick = function(){
         for(let j =0;j<botoes.length;j++){
         botoes[j].classList.remove('ativo')
-        }    
+        }   
+        
+        
         botoes[i].classList.add('ativo');
-        texto[i].classList.add('ativo');
+        textos[i].classList.add('ativo');
         
     }         
 }
