f81eb2d5dfe3501872ac6b032566186190dee6c9
Maria Lima <127319517+MeduuLima@users.noreply.github.com>
2024-04-23 07:58:53 -0300

    aaaaa

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ for(let i =0;i<botoes.length;i++){
         for(let j =0;j<botoes.length;j++){
         botoes[j].classList.remove('ativo')
         }    
+        botoes[i].classList.add('ativo');
+        texto[i].classList.add('ativo');
         
-    } 
-    botoes[i].classList.add('ativo');
-    texto[i].classList.add('ativo')         
+    }         
 }
