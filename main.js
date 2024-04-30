1fcbfd1e066ef6d892e84e3611b895961a008490
user <162068602+nataliainbox@users.noreply.github.com>
2024-04-30 07:54:03 -0300

    aaaa

--- main.js
+++ main.js
@@ -16,5 +16,5 @@ for(let i =0;i<botoes.length;i++){
 
 const contadores = document.querySelectorAll('.contador')
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00")
-const tempoAtual = new Date ()
+const tempoAtual = new Date()
 contadores[0].textContent = "Contagem regressiva"
