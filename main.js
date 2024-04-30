008ce413a67e539eca27358b2bd483e6d34e7707
user <162068602+nataliainbox@users.noreply.github.com>
2024-04-30 07:46:54 -0300

    .....

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ for(let i =0;i<botoes.length;i++){
     botoes[i].onclick = function(){
         for(let j =0;j<botoes.length;j++){
         botoes[j].classList.remove('ativo')
-        textos[j].classList.add('ativo')
+        
         }       
         botoes[i].classList.add('ativo');
         textos[i].classList.add('ativo')
