d0c52a1593f4a83c871eaff57dcff2d08f827be7
user <162068602+nataliainbox@users.noreply.github.com>
2024-05-07 08:02:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
-console.log(textos)
+
 for(let i =0;i<botoes.length;i++){
-    console.log(botoes[i])
+  
     botoes[i].onclick = function(){
         for(let j =0;j<botoes.length;j++){
-        botoes[j].classList.remove('ativo')
-        
+            console.log("textos[j]")
+            botoes[j].classList.remove('ativo')
+            textos[j].classList.remove('ativo')
+            console.log(textos[j])
         }       
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
@@ -18,4 +20,4 @@ const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date()
 
-contadores[0].textContent = (tempoObjetivo1 - tempoAtual)/1000/60/60/24
+contadores[0].textContent = ((tempoObjetivo1 - tempoatual)/1000)/60/60/24
