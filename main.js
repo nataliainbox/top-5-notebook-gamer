6d76227a8470a1beeabe631dbc12f056193284a9
user <162068602+nataliainbox@users.noreply.github.com>
2024-05-08 09:03:14 -0300

    .........

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
 
 for(let i =0;i<botoes.length;i++){
-  
+    console.log(botoes[i])
     botoes[i].onclick = function(){
         for(let j =0;j<botoes.length;j++){
             console.log("textos[j]")
