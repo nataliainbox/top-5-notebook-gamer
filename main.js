90b837badc2bb1450107edb9e30c6bcf16d784d0
user <162068602+nataliainbox@users.noreply.github.com>
2024-05-14 08:02:32 -0300

    .HJH

--- main.js
+++ main.js
@@ -19,5 +19,20 @@ for(let i =0;i<botoes.length;i++){
 const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date()
+contadores[0].textContent=calculaTempo(tempoObjetivo){
+let tempoAtual = new DataTransfer();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = 
+let minutos = 
+let horas = 
+let dias = Math.floor(horas / 24);
+}
+segundos %= 60
+minuto %= 60
+horas %= 24
 
-contadores[0].textContent = (tempoObjetivo1 - tempoAtual)/1000/60/60/24
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00")
+const tempoObjetivo3 = new Date("2023-12-05T00:00:00")
+const tempoObjetivo4 = new Date("2023-12-05T00:00:00")
+ 
