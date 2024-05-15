15cd0ee5ad750fe0fef5d747d8d820b58940b751
Maria Lima <127319517+MeduuLima@users.noreply.github.com>
2024-05-15 09:19:07 -0300

    gg

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ const textos = document.querySelectorAll(".aba-conteudo")
 for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
     botoes[i].onclick = function(){
-        for(let j =0;j<botoes.length;j++){
-            console.log("textos[j]")
+        for(let j =0;j<botoes.length;j++){        
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
-            console.log(textos[j])
         }       
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
-        
     }         
 }
 
